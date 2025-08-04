e7b1659448257d1f5eea15aa76171aeb961a3a17
smmasudrana <smmasudrana62@gmail.com>
2025-08-04 11:52:55 +0600

    add problem solving number 5

---
+++ Problem5.js
@@ -0,0 +1,5 @@
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+//write your code here
+var email = student.name + student.roll + '.' +student.department + '@ph.ac.bd';
+console.log(email); 
